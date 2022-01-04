53156d577cecba5f74894af39c01d7bab827bd1e
Rishabh Devbanshi <rdevbanshi35@gmail.com>
2022-01-04 20:27:51 +0530

    overlapping rectangles

---
+++ Milestone 1 - Goldman Sachs/overlapping rectangles.cpp
@@ -0,0 +1,13 @@
+int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        
+        //if either of rectangle is a line
+        if(L1[0] == R1[0] || L2[0] == R2[0] || L1[1] == R1[1] || L2[1] == R2[1])
+            return false;
+        
+        if(R1[0]<L2[0] || R2[0]<L1[0])
+            return false;
+        if(R1[1]>L2[1] || R2[1]>L1[1])
+            return false;
+            
+        return true;
+    }
